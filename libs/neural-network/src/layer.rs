6ac205753345e2a66e3c12543bb505cc770fc564
Carlton Wu <carltoncwujr@gmail.com>
2024-07-30 19:18:35 -0400

    - refactor neural network

---
+++ libs/neural-network/src/layer.rs
@@ -0,0 +1,55 @@
+use crate::*;
+
+#[derive(Clone, Debug)]
+pub struct Layer {
+    pub(crate) neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    pub fn random(rng: &mut dyn RngCore, input_size: usize, output_size: usize) -> Self {
+        let neurons = (0..output_size)
+                        .into_iter()
+                        .map(|_| Neuron::random(rng, input_size))
+                        .collect();
+
+        return Self{ neurons };
+    }
+
+    pub fn propogate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propogate(&inputs))
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn layer_random() {
+        let mut rng: ChaCha8Rng = ChaCha8Rng::from_seed(Default::default());
+        let layer = Layer::random(&mut rng, 4, 2);
+        
+        assert_relative_eq!(layer.neurons[1].bias, -0.53516835);
+        assert_relative_eq!(layer.neurons[1].weights.as_slice(),
+            [0.069369674, -0.7648182, -0.102499366, -0.48879617].as_ref());
+    }
+
+    #[test]
+    fn layer_propogate() {
+        let mut rng: ChaCha8Rng = ChaCha8Rng::from_seed(Default::default());
+        let layer = Layer::random(&mut rng, 4, 2);
+
+        assert_relative_eq!(layer.propogate(vec![0.5, 1.0, 0.25, 1.0]).as_slice(),
+            [(((0.67383957 * 0.5) + (0.8181262 * 1.0) + (0.26284897 * 0.25) + 
+            (0.5238807 * 1.0) + -0.6255188) as f32).max(0.0), 
+            (((0.069369674 * 0.5) + (-0.7648182 * 1.0) + (-0.102499366 * 0.25) + 
+            (-0.48879617 * 1.0) + -0.53516835) as f32).max(0.0)].as_ref());
+    }
+
+}
---
+++ libs/neural-network/src/neuron.rs
@@ -0,0 +1,68 @@
+use crate::*;
+
+#[derive(Clone, Debug)]
+pub struct Neuron {
+    pub(crate) bias: f32,
+    pub(crate) weights: Vec<f32>,
+}
+
+impl Neuron {
+    pub fn random(rng: &mut dyn RngCore, input_size: usize) -> Self {
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let weights = (0..input_size)
+                                        .into_iter()
+                                        .map(|_| rng.gen_range(-1.0..=1.0))
+                                        .collect();
+
+        return Self{ bias, weights };
+    }
+
+    pub fn propogate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+       let mut output = inputs
+                        .iter()
+                        .zip(&self.weights)
+                        .map(|(input, weight)| input * weight)
+                        .sum::<f32>();
+
+        output += self.bias;
+
+        return output.max(0.0);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::SeedableRng;
+    use rand_chacha::ChaCha8Rng;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn neuron_random() {
+        let mut rng = ChaCha8Rng::from_seed(Default::default());
+        let neuron = Neuron::random(&mut rng, 4);
+
+        assert_relative_eq!(neuron.bias, -0.6255188);
+        assert_relative_eq!(neuron.weights.as_slice(),
+            [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref());
+    }
+
+    #[test]
+    fn neuron_propogate() {
+        let neuron = Neuron {
+            bias: 0.5,
+            weights: vec![-0.3, 0.8],
+        };
+
+        assert_relative_eq!(neuron.propogate(&[-10.0, -10.0]),
+            0.0,
+        );
+
+        assert_relative_eq!(neuron.propogate(&[0.5, 1.0]),
+            (-0.3 * 0.5) + (0.8 * 1.0) + 0.5,
+        );
+    }
+}
